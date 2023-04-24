aebc5f0e2f54ee685dc6df15c1fa3c150250ef5f
Pankhuri-75 <84667167+Pankhuri-75@users.noreply.github.com>
2023-04-24 11:17:51 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Ecommerce")
+mongoose.connect("mongodb://mongo:27017/Ecommerce")
 .then(()=>{
     console.log("mongoDB connected");
 })
