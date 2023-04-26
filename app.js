6ec84ffa0c5f5a3f620c00cacecc536e264ec3e4
Pratheeksha P <84716592+Pratheeksha30@users.noreply.github.com>
2023-04-26 19:14:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,98 +1,247 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+var fs = require('fs');
+const hbs = require("hbs");
+var multer = require('multer');
 const users = require("./mongodb").users;
 const products = require("./mongodb").products;
 const carts = require("./mongodb").carts;
 const orders = require("./mongodb").orders;
 
-let user = null;
+let user = null, userName = null;
 let cartEmpty = true;
-
-var prod1, prod2, prod3, prod4, prod5, allProds;
-
-const getProds = async () => {
-    try {
-        prod1 = await products.findOne({ _id: 1 });
-        prod2 = await products.findOne({ _id: 2 });
-        prod3 = await products.findOne({ _id: 3 });
-        prod4 = await products.findOne({ _id: 4 });
-        prod5 = await products.findOne({ _id: 5 });
-
-        allProds = {
-            prod1: {
-                prodName: prod1.name,
-                prodDesc: prod1.description,
-                prodPrice: prod1.price
-            },
-            prod2: {
-                prodName: prod2.name,
-                prodDesc: prod2.description,
-                prodPrice: prod2.price
-            },
-            prod3: {
-                prodName: prod3.name,
-                prodDesc: prod3.description,
-                prodPrice: prod3.price
-            },
-            prod4: {
-                prodName: prod4.name,
-                prodDesc: prod4.description,
-                prodPrice: prod4.price
-            },
-            prod5: {
-                prodName: prod5.name,
-                prodDesc: prod5.description,
-                prodPrice: prod5.price
-            }
-        }
-    }
-    catch {
-        return;
-    }
-}
 
 const templatePath = path.join(__dirname, '/templates')
 
 app.use(express.json());
 app.set("view engine", "hbs");
 app.set("views", templatePath);
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper("str", function(e) {
+    return e.toString('base64');
+});
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/products')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+ 
+var upload = multer({ storage: storage });
+
+app.get('/adminHome', async (req, res) => {
+    try {
+        userArr = []
+        const allOrders = await orders.find({delivered : false});
+        if(allOrders.length){
+            for(var i = 0; i < allOrders.length; i++){
+                var use = await users.findOne({_id : allOrders[i].name});
+                userArr.push({
+                    _id : allOrders[i]._id,
+                    user_id : use._id,
+                    name : use.name,
+                    Add : use.address,
+                    Ph : use.phone,
+                    order : allOrders[i].order,
+                    total : allOrders[i].total,
+                    orderedDate : allOrders[i].orderedDate
+                })
+            }
+            res.render("adminHome", {Items : userArr});
+        }
+        else 
+        res.render("adminHome", {message: "Currently, there are no orders :("});
+    }
+    catch {
+        res.render("adminHome", {message: "Error occured while fetching orderd details !"});
+    }
+})
+
+app.get('/adminOH', async (req, res) => {
+    try {
+        userArr = []
+        const allOrders = await orders.find({delivered : true});
+        if(allOrders.length){
+            for(var i = 0; i < allOrders.length; i++){
+                var use = await users.findOne({_id : allOrders[i].name});
+                userArr.push({
+                    _id : allOrders[i]._id,
+                    user_id : use._id,
+                    name : use.name,
+                    Add : use.address,
+                    Ph : use.phone,
+                    order : allOrders[i].order,
+                    total : allOrders[i].total,
+                    orderedDate : allOrders[i].orderedDate,
+                    deliveredDate : allOrders[i].deliveredDate
+                })
+            }
+            res.render("adminOH", {Items : userArr});
+        }
+        else 
+        res.render("adminOH", {message: "Currently, there are no orders :("});
+    }
+    catch {
+        res.render("adminOH", {message: "Error occured while fetching orderd details !"});
+    }
+})
+
+app.post('/adminHome', async (req, res) => {
+    try {
+        var oID = req.body.oid;
+        let d = new Date();
+        var date = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear();
+        await orders.updateOne({_id : oID}, {$set : {delivered : true, deliveredDate : date}});
+        res.redirect("/adminHome");
+    }
+    catch {
+        res.render("adminHome", {message: "Failed to remove order !"});
+    }
+})
+
+app.get("/addProducts", (req,res) => {
+    res.render("addProducts");
+})
+
+app.get("/editProducts", (req,res) => {
+    res.render("editProducts");
+})
+
+app.get("/allProducts", async (req,res) => {
+    try {
+        const allProds = await products.find({});
+        res.render("allProducts", {items : allProds});
+    }
+    catch {
+        res.render("allProducts", {message : "Could not load the products :("});
+    }
+})
+
+app.post("/allProducts", async (req,res) => {
+    try {
+        const id = req.body.id;
+        await products.deleteOne({_id : id});
+        res.redirect("/allProducts");
+    }
+    catch {
+        res.render("allProducts", {message : "Could not delete the product :("});
+    }
+})
+
+app.post("/editProducts", async (req,res) => {
+    try {
+        let id = req.body.id;
+        var product = {
+            name: req.body.name,
+            category : req.body.category,
+            description: req.body.description,
+            price : req.body.price,
+            stock : req.body.stock
+        }
+        await products.updateOne( {_id : id} , { $set : product} );
+        res.redirect("/allProducts");
+    }
+    catch {
+        res.render("allProducts", {message : "Could not update the product :("});
+    }
+})
+
+app.post('/addProducts', upload.single('image'), async (req, res) => {
+    var product = {
+        _id : req.body.id,
+        name: req.body.name,
+        category : req.body.category,
+        description: req.body.description,
+        price : req.body.price,
+        stock : req.body.stock,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/public/products/' + req.file.originalname)),
+            contentType: 'image/png'
+        }
+    }
+    try{
+        await products.create(product);
+        res.redirect('/');
+    }
+    catch {
+        res.render("addProducts",{message : "Could not add new product !"});
+    }
+
+})
+
+app.post('/deleteProducts', async (req,res) => {
+    let pid = req.body.productID;
+    try {
+        await products.deleteOne({_id : pid});
+        res.redirect('/');
+    }
+    catch {
+        res.render('deleteProducts', {message: "Could not delete product!"})
+    }
+})
+
 app.get('/', async (req, res) => {
-    getProds();
-    res.render("home", allProds);
+    if(user && user == "admin"){
+        res.redirect("/adminHome");
+        return;
+    }
+    try {
+        const allProds = await products.find({});
+        res.render("home", {items : allProds});
+    }
+    catch {
+        res.render("home", {message : "Could not load the products :("});
+    }
 })
 
 app.post('/', async (req, res) => {
-    let id = req.body.id;
-    if (user) {
-        const check = await carts.findOne({ name: user });
-        if (check) {
-            if (!(id in check.items)) {
-                try {
-                    await carts.updateOne(
-                        { name: user },
-                        { $push: { items: id } });
-                }
-                catch {
-                    console.log("Error adding item! ");
+    if(!req.body.userSearch && req.body.userSearch != ""){    
+        let id = req.body.id;
+        if (user) {
+            const check = await carts.findOne({ name: user });
+            if (check) {
+                if (!(id in check.items)) {
+                    try {
+                        await carts.updateOne(
+                            { name: user },
+                            { $push: { items: id } });
+                    }
+                    catch {
+                        console.log("Error adding item! ");
+                    }
                 }
             }
+            else {
+                const newData = {
+                    name: user,
+                    items: [id]
+                };
+                await carts.insertMany([newData]);
+                cartEmpty = false;
+            }
+            res.redirect('/cart');
         }
         else {
-            const newData = {
-                name: user,
-                items: [id]
-            };
-            await carts.insertMany([newData]);
-            cartEmpty = false;
+            res.redirect('/login');
         }
-        res.redirect('/cart');
     }
-    else {
-        res.redirect('/login');
+    else{
+        //search
+        var userSearch = req.body.userSearch;
+        const results = await products.find({category : userSearch});
+        if(results){
+            if(results.length){
+                res.render("home", {items : results, message : "Search Results for " + userSearch});
+                return;
+            }
+        }
+        res.render("home", { message : "No Results found for " + userSearch});
     }
 })
 
@@ -113,7 +262,6 @@ app.get('/cart', async (req, res) => {
                 });
                 totalPrice += item.price;
             };
-            console.log(cartItems);
             res.render("cart", { items: cartItems, total: totalPrice, isnotMessage: true });
         }
         catch {
@@ -145,7 +293,7 @@ app.post('/cart', async (req, res) => {
         if (check) {
             let d = new Date();
             var date = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear();
-            await orders.insertMany([{ name: user, order: cartItems, total : totalPrice, orderedDate : date}]);
+            await orders.insertMany([{ name: user, order: cartItems, total : totalPrice, orderedDate : date, delivered : false}]);
             res.render("order", { items: cartItems, total: totalPrice,  isnotMessage: true });
             cartEmpty = true;
         }
@@ -160,16 +308,51 @@ app.post('/cart', async (req, res) => {
 
 app.get('/ordersList',async (req,res)=>{
     if(user){
-        // try {
-            let OrderItems = await orders.find({name : user});
+        try {
+            let OrderItems = await orders.find({name : user, delivered : false});
             OrderItems = Object.values(OrderItems);
-            if(OrderItems)
-            res.render("ordersList", { Items : OrderItems, isnotMessage: false })
-        // }
-        // catch {
+            if(OrderItems.length)
+            res.render("ordersList", { Items : OrderItems})
             else
-            res.render("ordersList",{ message: "You have not ordered anything yet !", isnotMessage: false })
-        // }
+            res.render("ordersList",{ message: "You have not ordered anything yet !"})
+        }
+        catch {
+            res.render("ordersList",{ message: "Could not load your orders :(" })
+        }
+    } 
+    else {
+        res.redirect('/login');
+    }
+})
+
+app.post('/ordersList',async (req,res)=>{
+    if(user){
+        try {
+            await orders.deleteOne({_id : req.body.oid});
+            res.redirect("/ordersList")
+        }
+        catch {
+            res.render("ordersList",{ message: "Could not load your orders :(" })
+        }
+    } 
+    else {
+        res.redirect('/login');
+    }
+})
+
+app.get('/userOH',async (req,res)=>{
+    if(user){
+        try {
+            let OrderItems = await orders.find({name : user, delivered : true});
+            OrderItems = Object.values(OrderItems);
+            if(OrderItems.length)
+            res.render("userOH", { Items : OrderItems})
+            else
+            res.render("userOH",{ message: "Nothing to show !"})
+        }
+        catch {
+            res.render("userOH",{ message: "Could not load your orders :(" })
+        }
     } 
     else {
         res.redirect('/login');
@@ -181,37 +364,44 @@ app.get('/login', (req, res) => {
         res.render("login.hbs");
     }
     else {
-        res.render('logout', { username: user });
+        res.render('logout', { username: userName });
     }
 })
 
 app.post("/login", async (req, res) => {
     if (!user) {
         try {
-            const check = await users.findOne({ name: req.body.name });
+            const check = await users.findOne({ _id: req.body.id });
             if (check.password == req.body.password) {
-                user = check.name;
-                if (!cartEmpty) {
-                    res.render("home", allProds);
+                user = check._id;
+                userName = check.name;
+                if(user != "admin"){
+                    if (!cartEmpty) {
+                        res.send("/");
+                    }
+                    else {
+                        res.redirect('/cart');
+                    }
                 }
                 else {
-                    res.redirect('/cart');
+                    res.redirect("/adminHome");
                 }
             } else {
                 res.render("login", { message: 'Incorrect password.' });
             }
         } catch {
-            res.render("login", { message: 'Incorrect Details.' });
+            res.render("login", { message: 'User does not exist.' });
         }
     } else {
-        res.render('logout', { username: user });
+        res.render('logout', { username : userName });
     }
 })
 
 app.post('/logout', (req, res) => {
     user = null;
+    userName = null;
     cartEmpty = true;
-    res.render("home", allProds);
+    res.redirect("/");
 })
 
 app.get('/signup', (req, res) => {
@@ -220,14 +410,19 @@ app.get('/signup', (req, res) => {
 
 app.post("/signup", async (req, res) => {
     const data = {
+        _id : req.body.id,
         name: req.body.name,
         password: req.body.password,
+        address : req.body.address,
+        phone : req.body.phone
     }
     await users.insertMany([data]);
-    user = req.body.name;
-    res.render("home");
+    user = req.body.id;
+    userName = req.body.name;
+    res.redirect("/");
 })
 
 app.listen(8000, () => {
     console.log("Application running on port 8000");
 })
+
--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const path = require("path");
+var fs = require('fs');
 
-mongoose.connect("mongodb://mongo:27017/Ecommerce")
+mongoose.connect("mongodb://localhost:27017/Ecommerce")
 .then(()=>{
     console.log("mongoDB connected");
 })
@@ -8,12 +10,24 @@ mongoose.connect("mongodb://mongo:27017/Ecommerce")
     console.log("Failed to connect");
 })
 
-const loginSchema = new mongoose.Schema({
-    name:{
+const userSchema = new mongoose.Schema({
+    _id : {
+        type:String,
+        required:true
+    }, 
+    name : {
         type:String,
         required:true
     },
-    password:{
+    password : {
+        type:String,
+        required:true
+    },
+    address : {
+        type:String,
+        required:true
+    },
+    phone : {
         type:String,
         required:true
     }
@@ -28,6 +42,10 @@ const productSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    category : {
+        type:String,
+        required:true
+    },
     description:{
         type:String,
         required:true
@@ -39,6 +57,10 @@ const productSchema = new mongoose.Schema({
     stock:{
         type:Number,
         required:true        
+    },
+    img : {
+        data: Buffer,
+        contentType: String
     }
 })
 
@@ -69,10 +91,17 @@ const orderSchema = new mongoose.Schema({
     orderedDate : {
         type:String,
         required:true
+    },
+    delivered : {
+        type:Boolean,
+        required:true
+    },
+    deliveredDate : {
+        type:String
     }
 })
 
-const users = new mongoose.model("Users",loginSchema);
+const users = new mongoose.model("Users",userSchema);
 const products = new mongoose.model("Products",productSchema);
 const carts = new mongoose.model("Carts",cartSchema);
 const orders = new mongoose.model("Orders",orderSchema);
@@ -80,48 +109,101 @@ const orders = new mongoose.model("Orders",orderSchema);
 const addProducts = async () => {
     data  = [{
         _id : 1,
-        name : "Kurti",
-        description : "Red Color kurti.\nTailored with three quarter sleeves and a round neck",
+        name : "Red Print Kurta",
+        category : "kurta",
+        description : "Red Color kurta.\nTailored with three quarter sleeves and a round neck",
         price : 899,
-        stock : 2
+        stock : 2,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/public/products/1.png')),
+            contentType: 'image/png'
+        }
     }, {
         _id : 2,
         name : "Blue Print Skirt",
+        category : "skirt",
         description : "Blue kalidar lehenga skirt in georgette.\nIt features chevron foil print and tassels.\nFinished with a shantoon lining, and a zip closure.",
         price : 3600,
-        stock : 2
+        stock : 2,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/public/products/2.png')),
+            contentType: 'image/png'
+        }
     }, {
         _id : 3,
         name : "Black Print Kurta",
+        category : "kurta",
         description : "Black tiered maxi kurta in viscose.\nTailored with three quarter sleeves and a round neck, it features an all-over boota print, mirror embroidery and a dori tie up on the front.\nFinished with a zip closure and two pockets.",
-        price : 599,
-        stock : 2
+        price : 799,
+        stock : 2,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/public/products/3.png')),
+            contentType: 'image/png'
+        }
     }, {
         _id : 4,
         name : "Floral Midi Dress",
-        description : "Blue maxi dress in georgette.\nTailored sleeveless with a V-neck, it features all-over floral print.\nFinished with shantoon lining and a zip closure.",
+        category : "dress",
+        description : "Blue maxi dress in georgette. Tailored sleeveless with a V-neck, it features all-over floral print.\nFinished with shantoon lining and a zip closure.",
         price : 2599,
-        stock : 2
+        stock : 2,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/public/products/4.png')),
+            contentType: 'image/png'
+        }
     }, {
         _id : 5,
         name : "Mughal Print Lehenga",
+        category : "lehenga",
         description : "Ivory kalidar lehenga in georgette.\nIt features an all-over floral print and mughal border.\nFinished with a shantoon lining and a zip closure.",
         price : 3560,
-        stock : 2
+        stock : 2,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/public/products/5.png')),
+            contentType: 'image/png'
+        }
     }]
     try{
-        const check = await products.insertMany(data);
-        if(check){
-            console.log("Uploaded Successfully !");
+        for(var i = 0 ; i < 5; i++){
+            try{
+                let check = await products.create(data[i]);
+                if(check){
+                    console.log("Uploaded Products Data Successfully.");
+                }
+            }
+            catch {
+                console.log("Could not upload file.")
+            }
         }
         return;
     }
     catch {
-        console.log("Error uploading data to database !");
+        console.log("Products data already exists !");
     }
     return;
 }
 
+const addAdmin = async () => {
+    data = {
+        _id : "admin",
+        name : "admin",
+        password : "admin",
+        address : " ",
+        phone : " "
+    };
+    try {
+        const check = await users.insertMany([data]);
+        if(check){
+            console.log("Admin Info Added Successfully.");
+        }
+    }
+    catch {
+        console.log("Admin data already exists !");
+    }
+    return;
+}
+
+addAdmin();
 addProducts();
 
 module.exports={users,products,carts,orders};
